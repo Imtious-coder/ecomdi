8112fd890f66605291dfc7b7bc0aefabe555211f
unknown <iarafat386@gmail.com>
2021-06-01 15:58:27 +0600

    Security and Login page complete

--- src/Componnents/My_Account/SecurityLogin/SecurityLogin.js
+++ src/Componnents/My_Account/SecurityLogin/SecurityLogin.js
@@ -54,31 +54,58 @@ const SecurityLogin = () => {
                         </button>
                     </div>
                 </div>
-                {/* Name */}
+                {/* Password */}
                 <div className="grid grid-cols-2 ml-10 mr-10 mb-10 pt-5 border rounder">
                     <div className="ml-10 mb-5">
-                        <p className="font-bold">Name:</p>
-                        <p>Yusuf Islam</p>
+                        <p className="font-bold">Country:</p>
+                        <p>Bangladesh</p>
                     </div>
                     <div className="ml-20">
-                        <button className=" mt-2 rounded Button1">
+                        <button className="ml-5 mt-2 rounded Button1">
                             Edit
                         </button>
                     </div>
                 </div>
-                {/* Email */}
+                {/* Address */}
                 <div className="grid grid-cols-2 ml-10 mr-10 mb-10 pt-5 border rounder">
                     <div className="ml-10 mb-5">
-                        <p className="font-bold">Email:</p>
-                        <p>Yusuf.Islam@gmail.com</p>
+                        <p className="font-bold">Address:</p>
+                        <p>Dhaka, Bangladesh</p>
                     </div>
                     <div className="ml-20">
-                        <button className=" mt-2 rounded Button1">
+                        <button className="ml-5 mt-2 rounded Button1">
                             Edit
                         </button>
                     </div>
                 </div>
-                
+            </section>
+            {/* Securitys cards..... */}
+            <section className="grid md:grid-cols-2 items-center">
+                {/* Two Steps Verification */}
+                <div className="grid grid-cols-2 ml-10 mr-10 mb-10 pt-7 pb-3 border rounder">
+                    <div className="-mr-20 ml-10 mb-5">
+                        <p className="font-bold">Two-Step Verification (2SV) Settings:</p>
+                        <p>Manage your Two Step Verification (2SV) Authenticators</p>
+                    </div>
+                    <div className="ml-10 flex justify-end content-center flex-wrap">
+                        <button className="rounded Button1 -mt-4 mr-5">
+                            Edit
+                        </button>
+                    </div>
+                </div>
+                {/* Secure account */}
+                <div className="grid grid-cols-2 ml-10 mr-10 mb-10 pt-5 border rounder">
+                    <div className="-mr-20 ml-10 mb-5">
+                        <p className="font-bold">Secure Your Account:</p>
+                        <p>If you think your Amazon account has been compromised, follow these steps to make your account more secure.</p>
+                    </div>
+                    <div className="ml-10 flex justify-end content-center flex-wrap">
+                        <button className="rounded Button1 -mt-4 mr-5">
+                            Edit
+                        </button>
+                    </div>
+                </div>
+
             </section>
         </section>
     );
