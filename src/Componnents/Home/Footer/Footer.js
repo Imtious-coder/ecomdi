549d0dff9201daea2c140fad402610f9b0ed0062
unknown <iarafat386@gmail.com>
2021-06-24 19:08:54 +0600

    Footer Updated

--- src/Componnents/Home/Footer/Footer.js
+++ src/Componnents/Home/Footer/Footer.js
@@ -8,39 +8,39 @@ const Footer = () => {
                 <a href="" className="text-xl text-gray-300"><p><b>Get to Know Us</b></p></a>
                 <a href=""><p className="text-gray-400">Careers</p></a>
                 <a href=""><p className="text-gray-400">Blogs</p></a>
-                <a href=""><p className="text-gray-400">About ecomdi</p></a>
+                <a href=""><p className="text-gray-400">About Ecomdi</p></a>
                 <a href=""><p className="text-gray-400">Sustainability</p></a>
                 <a href=""><p className="text-gray-400">Press Center</p></a>
                 <a href=""><p className="text-gray-400">Investor Relations</p></a>
-                <a href=""><p className="text-gray-400">Amazon Devices</p></a>
+                <a href=""><p className="text-gray-400">Ecomdi Devices</p></a>
             </div>
             <div className="text-white text-sm m-10 leading-5 text-gray-300">
                 <a href="" className="text-xl text-gray-300"><p><b>Make Money with Us</b></p></a>
-                <a href=""><p className="text-gray-400">Sell products on ecomdi</p></a>
-                <a href=""><p className="text-gray-400">Sell apps on ecomdi</p></a>
+                <a href=""><p className="text-gray-400">Sell products on Ecomdi</p></a>
+                <a href=""><p className="text-gray-400">Sell apps on Ecomdi</p></a>
                 <a href=""><p className="text-gray-400">Become an Affiliate</p></a>
                 <a href=""><p className="text-gray-400">Become a Delivery Driver</p></a>
                 <a href=""><p className="text-gray-400">Start a package delivery business</p></a>
                 <a href=""><p className="text-gray-400">Advertise Your Products</p></a>
                 <a href=""><p className="text-gray-400">Self-Publish with Us</p></a>
-                <a href=""><p className="text-gray-400">Host an ecomdi Hub</p></a>
+                <a href=""><p className="text-gray-400">Host an Ecomdi Hub</p></a>
                 <a href=""><p className="text-gray-400">› See More Make Money with Us</p></a>
             </div>
             <div className="text-white text-sm m-10 leading-5 text-gray-300">
                 <a href="" className="text-xl text-gray-300"><p><b>Ecomdi Payment Products</b></p></a>
-                <a href=""><p className="text-gray-400">ecomdi Making payments</p></a>
+                <a href=""><p className="text-gray-400">Ecomdi Making payments</p></a>
                 <a href=""><p className="text-gray-400">Blogs</p></a>
-                <a href=""><p className="text-gray-400">ecomdi.com Store Card</p></a>
-                <a href=""><p className="text-gray-400" className="text-gray-400">ecomdi Secured Card</p></a>
-                <a href=""><p className="text-gray-400">ecomdi Business Card</p></a>
-                <a href=""><p className="text-gray-400">ecomdi Business Line of Credit</p></a>
+                <a href=""><p className="text-gray-400">Ecomdi.com Store Card</p></a>
+                <a href=""><p className="text-gray-400" className="text-gray-400">Ecomdi Secured Card</p></a>
+                <a href=""><p className="text-gray-400">Ecomdi Business Card</p></a>
+                <a href=""><p className="text-gray-400">Ecomdi Business Line of Credit</p></a>
                 <a href=""><p className="text-gray-400">Shop with Points</p></a>
                 <a href=""><p className="text-gray-400">Reload Your Balance</p></a>
-                <a href=""><p className="text-gray-400">ecomdi Currency Converter</p></a>
+                <a href=""><p className="text-gray-400">Ecomdi Currency Converter</p></a>
             </div>
             <div className="text-white text-sm m-10 leading-5 text-gray-300">
                 <a href="" className="text-xl text-gray-300"><p><b>Let Us Help You</b></p></a>
-                <a href=""><p className="text-gray-400">Amazon and COVID-19</p></a>
+                <a href=""><p className="text-gray-400">Ecomdi and COVID-19</p></a>
                 <a href=""><p className="text-gray-400">Blogs</p></a>
                 <a href=""><p className="text-gray-400">Your Account</p></a>
                 <a href=""><p className="text-gray-400">Your Orders</p></a>
