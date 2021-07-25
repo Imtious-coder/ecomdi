6eebf24c88a416cf265662321a1eef249e827e1b
unknown <iarafat386@gmail.com>
2021-07-25 16:59:46 +0600

    Problem solved

--- src/Componnents/Categories/CategoriesNavigation/CategoriesNavigation.js
+++ src/Componnents/Categories/CategoriesNavigation/CategoriesNavigation.js
@@ -1,6 +1,5 @@
 import React from 'react';
-import { Menu  } from 'antd';
-import { Link } from 'react-router-dom';
+import { Menu } from 'antd';
 import './CategoriesNavigation.css';
 
 const { SubMenu } = Menu;
@@ -14,238 +13,60 @@ const CategoriesNavigation = () => {
         <section className="Tes">
             <Menu onClick={handleClick} mode="vertical">
                 <SubMenu title="Mobiles & Laptop">
-                    <Menu.Item className="loll">
-                    <Link  to='/mobile'>
-                            All Phone
-                    </Link>
-                    </Menu.Item>
-                    <Menu.Item  className="loll">
-                    <Link  to='/laptop'>
-                            All Laptops
-                    </Link>
-                        </Menu.Item>
-                    <Menu.Item  className="loll">
-                    <Link  to='/nokia'>
-                        Nokia
-                    </Link>
-                    </Menu.Item>
-                    <Menu.Item  className="loll">
-                    <Link  to='/blackberry'>
-                        Blackberry
-                    </Link>
-                    </Menu.Item>
-                    <Menu.Item  className="loll">
-                    <Link  to='/realme'>
-                        Realme
-                    </Link>
-                    </Menu.Item>
-                    <Menu.Item >
-                    <Link  to='/samsung'>
-                        SamSung
-                    </Link>
-                    </Menu.Item>
-                    <Menu.Item >
-                    <Link  to='/walton'>
-                        Walton
-                    </Link>
-                    </Menu.Item>
-                    <Menu.Item >
-                    <Link  to='/iphone'>
-                        Iphone
-                    </Link>
-                    </Menu.Item>
-                    <Menu.Item >
-                    <Link  to='/android'>
-                        Android
-                    </Link>
-                    </Menu.Item>
-                    <Menu.Item >
-                    <Link  to='/asus'>
-                        Asus
-                    </Link>
-                    </Menu.Item>
-                    <Menu.Item >
-                    <Link  to='/Lenovo'>
-                        Lenovo
-                    </Link>
-                    </Menu.Item>
-                    <Menu.Item >
-                    <Link  to='/macbook'>
-                        Macbook
-                    </Link>
-                    </Menu.Item>
-                    <Menu.Item >
-                    <Link  to='/dell'>
-                        Dell
-                    </Link>
-                    </Menu.Item>
-                    <Menu.Item >
-                    <Link  to='/hp'>
-                        Hp
-                    </Link>
-                    </Menu.Item>
+                    <Menu.Item>All Phone</Menu.Item>
+                    <Menu.Item>All Laptops</Menu.Item>
+                    <Menu.Item>Nokia</Menu.Item>
+                    <Menu.Item>Blackberry</Menu.Item>
+                    <Menu.Item>Realme</Menu.Item>
+                    <Menu.Item >Samsung</Menu.Item>
+                    <Menu.Item >Walton</Menu.Item>
+                    <Menu.Item >iphone</Menu.Item>
+                    <Menu.Item >Android</Menu.Item>
+                    <Menu.Item >Asus</Menu.Item>
+                    <Menu.Item >Lenovo</Menu.Item>
+                    <Menu.Item >Mackbook</Menu.Item>
+                    <Menu.Item >Dell</Menu.Item>
+                    <Menu.Item >Hp</Menu.Item>
                 </SubMenu>
                 <SubMenu title="Bick & Scooter">
                     <SubMenu title="Bicks">
-                        <Menu.Item >
-                            <Link  to='/bikes'>
-                            All Bicks
-                            </Link>
-                        </Menu.Item>
-                        <Menu.Item >
-                        <Link  to='/tvs'>
-                            TVS
-                        </Link>
-                        </Menu.Item>
-                        <Menu.Item >
-                            <Link  to='/suzuki'>
-                            Suzuki
-                            </Link>
-                            </Menu.Item>
-                        <Menu.Item >
-                        <Link  to='/hero'>
-                            Hero
-                            </Link>
-                        </Menu.Item>
-                        <Menu.Item >
-                        <Link  to='/yamaha'>
-                            Yamaha
-                            </Link>
-                        </Menu.Item>
-                        <Menu.Item >
-                        <Link  to='/honda'>
-                            Honda
-                            </Link>
-                        </Menu.Item>
-                        <Menu.Item >
-                        <Link  to='/comuter-bike'>
-                            Comuter Bike
-                            </Link>
-                        </Menu.Item>
-                        <Menu.Item >
-                        <Link  to='/sports-bike'>
-                            Sports Bike
-                            </Link>
-                        </Menu.Item>
-                        <Menu.Item >
-                        <Link  to='/nacked-bike'>
-                            Nacked Bike
-                            </Link>
-                        </Menu.Item>
+                        <Menu.Item >All Bicks</Menu.Item>
+                        <Menu.Item >TVS</Menu.Item>
+                        <Menu.Item >Suzuki</Menu.Item>
+                        <Menu.Item >Hero</Menu.Item>
+                        <Menu.Item >Yamaha</Menu.Item>
+                        <Menu.Item >Honda</Menu.Item>
+                        <Menu.Item >Comuter Bick</Menu.Item>
+                        <Menu.Item >Sports Bick</Menu.Item>
+                        <Menu.Item >Nacked Bicks</Menu.Item>
                     </SubMenu>
-                    <Menu.Item >
-                        <Link  to='/scooter'>
-                            Scooter
-                            </Link>
-                        </Menu.Item>
+                    <Menu.Item >Scooter</Menu.Item>
                 </SubMenu>
                 <SubMenu title="Groceries">
-                    <Menu.Item >
-                        <Link to = './groceries'>
-                          All Groceries
-                        </Link>
-                    </Menu.Item>
-                    <Menu.Item >
-                    <Link to = './oil'>
-                          Oil
-                        </Link>
-                    </Menu.Item>
-                    <Menu.Item >
-                    <Link to = './chal'>
-                          Chal
-                        </Link>
-                    </Menu.Item>
-                    <Menu.Item >
-                    <Link to = './spices'>
-                          Spices
-                        </Link>
-                    </Menu.Item>
-                    <Menu.Item >
-                    <Link to = './dry-fruits'>
-                          Dry Fruits
-                        </Link>
-                    </Menu.Item>
+                    <Menu.Item >Oil</Menu.Item>
+                    <Menu.Item >Chal</Menu.Item>
+                    <Menu.Item >Spices</Menu.Item>
+                    <Menu.Item >Dry Fruits</Menu.Item>
                 </SubMenu>
-                <Menu.Item >
-                <Link to = './television'>
-                          Television
-                        </Link>
-                </Menu.Item>
-                <Menu.Item >
-                <Link to = './security-camera'>
-                          Security Camera
-                        </Link>
-                </Menu.Item>
-                <Menu.Item >
-                <Link to = './smart-gadgets'>
-                          Smart Gadgets
-                        </Link>
-                </Menu.Item>
-                <Menu.Item >
-                <Link to = './health-and-beauty'>
-                          Health & Beauty
-                        </Link>
-                </Menu.Item>
-                <Menu.Item >
-                <Link to = './men-fashion'>
-                    Men's Fashion
-                 </Link>
-                </Menu.Item>
-                <Menu.Item >
-                <Link to = './women-fashion'>
-                    Women's Fashion
-                  </Link>
-                </Menu.Item>
+                <Menu.Item >Television</Menu.Item>
+                <Menu.Item >Security cameras</Menu.Item>
+                <Menu.Item >Health & Beauty</Menu.Item>
+                <Menu.Item >Man's Fasion</Menu.Item>
+                <Menu.Item >Women's Fasion</Menu.Item>
 
                 <SubMenu title="Home & Lifestyle">
-                <   Menu.Item >
-                    <Link to = './home-accecories'>
-                         Home Accecories
-                    </Link>
-                    </Menu.Item>
-                    <Menu.Item >
-                    <Link to = './kitchen'>
-                         Kitchen
-                    </Link>
-                    </Menu.Item>
-                    <Menu.Item >
-                    <Link to = './blender'>
-                         Blender
-                    </Link>
-                    </Menu.Item>
-                    <Menu.Item >
-                    <Link to = './water-filter'>
-                         Water Filter
-                    </Link>
-                    </Menu.Item>
-                    <Menu.Item >
-                    <Link to = './chair-table'>
-                         Chairs & Tables
-                    </Link>
-                    </Menu.Item>
-                    <Menu.Item >
-                    <Link to = './doormats'>
-                         Doormats
-                    </Link>
-                    </Menu.Item>
+                    <Menu.Item >Home accecories</Menu.Item>
+                    <Menu.Item >Kitchen Items</Menu.Item>
+                    <Menu.Item >Door Mats</Menu.Item>
+                    <Menu.Item >Water Filter</Menu.Item>
+                    <Menu.Item >Blender</Menu.Item>
+                    <Menu.Item >Water Filter</Menu.Item>
+                    <Menu.Item >Tables & Chair</Menu.Item>
                 </SubMenu>
-                <Menu.Item >
-                <Link to = './babies-toys'>
-                          Babies & Toys
-                        </Link>
-                </Menu.Item>
-                <Menu.Item >
-                <Link to = './sports'>
-                          Sports
-                        </Link>
-                </Menu.Item>
-                <Menu.Item >
-                <Link to = './touring'>
-                          Touring
-                        </Link>
-                </Menu.Item>
-                
-
+                <Menu.Item >Babies & Toys</Menu.Item>
+                <Menu.Item >Sports</Menu.Item>
+                <Menu.Item >Touring</Menu.Item>
+                <Menu.Item >Smart Gadgets</Menu.Item>
             </Menu>
         </section>
     );
